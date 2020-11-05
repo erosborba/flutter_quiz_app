1c7e0abbaa562631aaa473f7a6356d210b031b39
Eros Borba <borba.eros@gmail.com>
2020-11-04 21:37:43 -0300

    Add constants file

---
+++ lib/constanst.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFb3e9c7);
+const kPrimaryColorActive = Color(0xFF5603ad);
+const kSecondaryColor = Color(0xFF28367c7);
+const kSecondaryColorActive = Color(0xFFc2f8cb);
+const kprimaryTextColor = Color(0xff2c2a4a);
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+import 'constanst.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: kPrimaryColorActive,
+          title: Text("Quiz App"),
+        ),
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              RaisedButton(
+                child: Text("Question One"),
+                color: kSecondaryColorActive,
+                textColor: kprimaryTextColor,
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
